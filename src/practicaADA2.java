cc7e0ad92af75f055448d4314a37d925a6e13450
oscar90y5 <oscarfernandezangulo@gmail.com>
2016-11-08 23:37:22 +0100

    añadida solucion de silvia (dinamica), integrado y comenzada la depuracion (sin probar)

--- src/practicaADA2.java
+++ src/practicaADA2.java
@@ -34,6 +34,17 @@ public class practicaADA2 {
             i++;
         }
 
+        //Solucion silvia
+        int solsubset[]=new int [subConjunto.size()];
+        boolean tabla[][] = tableSubsetSum(subConjunto, sum);
+        if (tabla[sum][subConjunto.size()]==false)
+            System.out.println("No hay una subsecuencia que sume sum");
+        else{
+            imprimirTabla(tabla);
+            iniciarsolsubset(solsubset);
+            calcularSolSubSet(solsubset,subConjunto,tabla,sum,solsubset.length);
+        }
+        //finnnnn
 
         fichero = new FileWriter("../practicaADA2/solucion.txt");
         pw = new PrintWriter(fichero);
@@ -100,5 +111,75 @@ public class practicaADA2 {
         }
 
     }
+
+    static void calcularSolSubSet(int solsubset[], ArrayList<Integer> set, boolean tabla[][], int sum, int n) {
+
+        if(( sum == 0)) {
+            imprimirSolSubSet(solsubset);
+            iniciarsolsubset(solsubset);
+        }
+
+        if(n>0) {
+
+            if ((set.get(n - 1) <= sum)&&(tabla[sum][n]==true)) {
+                calcularSolSubSet(solsubset, set, tabla, sum, n - 1);
+                solsubset[n - 1] = set.get(n - 1);
+                calcularSolSubSet(solsubset, set, tabla, sum - set.get(n - 1), n - 1);
+
+            } else {
+                calcularSolSubSet(solsubset, set, tabla, sum, n - 1);
+
+            }
+
+        }
+
+    }
+
+    static boolean[][] tableSubsetSum(ArrayList<Integer> set, int sum) {
+        int i, j;
+        // el valor del subset[i][j] es true si hay
+        // un subset del set[0..j-1] con la suma igual a i
+        boolean subset[][] = new boolean[sum + 1][set.size() + 1];
+        // si sum es 0, es true
+        for (i = 0; i <= set.size(); i++)
+            subset[0][i] = true;
+        // si la suma no es 0 y la secuencia está facía, es falso
+        for (i = 1; i <= sum; i++)
+            subset[i][0] = false;
+        // Llena la tabla de subsecuencias
+        for (i = 1; i <= sum; i++) {
+            for (j = 1; j <= set.size(); j++) {
+                subset[i][j] = subset[i][j - 1];
+                if (i >= set.get(j - 1))
+                    subset[i][j] = subset[i][j] || subset[i - set.get(j - 1)][j - 1];
+            }
+        }
+        return subset;
+    }
+
+    static void iniciarsolsubset(int solsubset[]) {
+        for (int i = 0; i < solsubset.length; i++) {
+            solsubset[i] = 0;
+        }
+    }
+
+    static void imprimirSolSubSet(int solsubset[]){
+        System.out.print("Subsecuencia: [");
+        for (int i = 0; i < solsubset.length; i++){
+            if (solsubset[i]!=0)
+                System.out.print(solsubset[i]+",");
+        }
+        System.out.println("]");
+    }
+
+    public static void imprimirTabla(boolean tabla[][]){
+        for (int i = 0; i <= tabla.length; i++) {
+            for (int j = 0; j <= tabla[i].length; j++) {
+                System.out.printf("%4d", (tabla[i][j]) ? 1 : 0);
+            }
+            System.out.printf("\n");
+        }
+    }
+
 
 }
